d100ec1b0a5a83dbbf68b876e6898d29a3e55e90
Neel Doshi <neel.doshi@sky.uk>
2022-06-09 15:11:26 +0100

    fix console errors

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -1,10 +1,26 @@
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
+
+// const Meta = ({ title }) => {
+//   return (
+//     <Helmet>
+//       <title>{title}</title>
+//     </Helmet>
+//   );
+// };
+
+//export default Meta;
+
+import React from "react";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const Meta = ({ title }) => {
   return (
-    <Helmet>
-      <title>{title}</title>
-    </Helmet>
+    <HelmetProvider>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <h1>Hello World</h1>
+    </HelmetProvider>
   );
 };
 
